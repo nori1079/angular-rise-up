85deaa371af47a71143e1de02b37e7b8db01b74d
yoshinori <koro1079@yahoo.co.jp>
2019-12-02 11:31:42 +0900

    feat(); sameを用いたレベル計算

--- functions/src/git-hook.function.ts
+++ functions/src/git-hook.function.ts
@@ -5,14 +5,41 @@ admin.initializeApp();
 
 const db = admin.firestore();
 
+const expTable = [
+  20,
+  40,
+  100,
+  250,
+  500,
+  1000,
+  1500,
+  4000,
+  10000
+];
+
+const EARN_EXPERIENCE = 10;
+
 export const gitHook = functions.https.onRequest(async (request, response) => {
   const actions = await db.collection('actions')
     .where('ownerGitHubId', '==', request.body.sender.id)
-    .get()
+    .get();
 
-  const increement = admin.firestore.FieldValue.increment(10);
+  const action = actions.docs[0].data();
+
+  let level = 1;
+  expTable.some(nextExp => {
+    if (action.exp + EARN_EXPERIENCE > nextExp) {
+      level++;
+      return false;
+    } else {
+      return true;
+    }
+  });
+
+  const increement = admin.firestore.FieldValue.increment(EARN_EXPERIENCE);
   actions.docs.forEach(doc => doc.ref.update({
-    exp: increement
-  }))
+    exp: increement,
+    level
+  }));
   response.send('success');
 });
